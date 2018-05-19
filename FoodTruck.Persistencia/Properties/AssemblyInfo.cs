9b2b94df090e99773d80db205a1eeea31f69c03b
amiilto <39378921+amiilto@users.noreply.github.com>
2018-05-19 16:10:31 -0300

    Criação de classes e métodos
    Adicionadas as classes principais do sistema, bem como as suas variáveis e métodos.

--- FoodTruck.Persistencia/Properties/AssemblyInfo.cs
+++ FoodTruck.Persistencia/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FoodTruck.Telas")]
+[assembly: AssemblyTitle("FoodTruck.Persistencia")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FoodTruck.Telas")]
+[assembly: AssemblyProduct("FoodTruck.Persistencia")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("23d6d4fa-0e9c-43e7-974d-4bf4dfa7503e")]
+[assembly: Guid("d665223f-fc4d-4cfa-b722-ff9ebf3a53d0")]
 
 // Version information for an assembly consists of the following four values:
 //
