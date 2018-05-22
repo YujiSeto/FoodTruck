efb12f227b97663ffe4bbcb26db18c5a76b9baab
amiilto <39378921+amiilto@users.noreply.github.com>
2018-05-22 15:52:20 -0300

    Adição das telas e métodos Lanches (Exercício)
    Adição das telas gráficas e dos métodos para adicionar, remover e alterar lanches.

---
+++ FoodTruck.Grafico/ManterLanche.cs
@@ -0,0 +1,79 @@
+﻿using FoodTruck.Negocio;
+using FoodTruck.Negocio.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodTruck.Grafico
+{
+    public partial class ManterLanche : Form
+    {
+        public Lanche LancheSelecionado { get; set; }
+
+        public ManterLanche()
+        {
+            InitializeComponent();
+        }
+
+        private void btSalvar_Click(object sender, EventArgs e)
+        {
+            Lanche lanche = new Lanche();
+            lanche.Id = Convert.ToInt64(tbId.Text);
+            lanche.Nome = tbNome.Text;
+            lanche.Valor = Convert.ToDecimal(tbValor.Text);
+
+            Validacao validacao;
+            if (LancheSelecionado == null)
+            {
+                validacao = Program.Gerenciador.CadastrarLanche(lanche);
+            }
+            else
+            {
+                validacao = Program.Gerenciador.AlterarLanche(lanche);
+            }
+
+            if (!validacao.Valido)
+            {
+                String mensagemValidacao = "";
+                foreach(var chave in validacao.Mensagens.Keys)
+                {
+                    String msg = validacao.Mensagens[chave];
+                    mensagemValidacao += msg;
+                    mensagemValidacao += Environment.NewLine;
+                }
+                MessageBox.Show(mensagemValidacao);
+            }
+            else
+            {
+                MessageBox.Show("Lanche salvo com sucesso!");
+            }
+            this.Close();
+        }
+
+        private void btCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ManterLanche_Shown(object sender, EventArgs e)
+        {
+            if(LancheSelecionado != null)
+            {
+                this.tbId.Text = LancheSelecionado.Id.ToString();
+                this.tbNome.Text = LancheSelecionado.Nome;
+                this.tbValor.Text = LancheSelecionado.Valor.ToString();
+            }
+        }
+
+        private void ManterLanche_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
